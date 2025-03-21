8566e1db712af2c44548ff2fbe0fa1f4a1a7eaef
sora5042 <esuora5042@gmail.com>
2025-03-21 16:31:52 +0900

    Initial Commit

---
+++ CalendarTests/CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTests.swift
+//  CalendarTests
+//
+//  Created by Sora Oya on 2025/03/21.
+//
+
+import Testing
+@testable import Calendar
+
+struct CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
