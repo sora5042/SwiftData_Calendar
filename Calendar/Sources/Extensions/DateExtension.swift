62d5a8f577318f73f1966edb6afcf16b2029579b
sora5042 <esuora5042@gmail.com>
2025-04-14 01:04:57 +0900

    Add CalendarViewModel for managing calendar data

---
+++ Calendar/Sources/Extensions/DateExtension.swift
@@ -0,0 +1,42 @@
+//
+//  DateExtension.swift
+//  Calendar
+//
+//  Created by Sora Oya on 2025/04/07.
+//
+
+import Foundation
+
+extension Date {
+    
+    init?(string: String, format: String) {
+        guard let date = DateFormatter(dateFormat: format).date(from: string) else {
+            return nil
+        }
+        self = date
+    }
+
+    func format(_ dateFormat: String = "yyyy-MM-dd HH:mm:ss") -> String {
+        formatted(dateFormat)
+    }
+
+    func formatted(_ dateFormat: String = "yyyy-MM-dd HH:mm:ss") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        dateFormatter.timeZone = TimeZone.current
+        return dateFormatter.string(from: self)
+    }
+}
+
+extension DateFormatter {
+    convenience init(calendar: Calendar.Identifier, locale: Locale = .init(identifier: "en_US_POSIX")) {
+        self.init()
+        self.calendar = .init(identifier: calendar)
+        self.locale = locale
+    }
+
+    convenience init(dateFormat: String) {
+        self.init(calendar: .gregorian)
+        self.dateFormat = dateFormat
+    }
+}
---
+++ Calendar/Sources/ViewModels/CalendarViewModel.swift
@@ -0,0 +1,104 @@
+//
+//  CalendarViewModel.swift
+//  Calendar
+//
+//  Created by Sora Oya on 2025/04/07.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+final class CalendarViewModel: ObservableObject {
+    private let todoRepository: TodoRepository
+
+    @Published
+    var selectedDates: Set<DateComponents> = [Calendar.current.dateComponents([.calendar, .era, .year, .month, .day], from: Date())]
+
+    var date: Date {
+        guard let selectedDate = selectedDates.first else { return .now }
+        return Calendar.current.date(from: selectedDate) ?? .now
+    }
+
+    private var todos: [Todo] = []
+    
+    @Published
+    var displayTodos: [Todo] = []
+
+    @Published
+    var sheet: Sheet?
+
+    init(
+        todoRepository: TodoRepository = .init(context: .init(ModelContainerManager.shared.modelContainer))
+    ) {
+        self.todoRepository = todoRepository
+    }
+
+    func fetchTodos() async {
+        let todos = await todoRepository.fetchTodos()
+        print(todos)
+        self.todos = todos.map(Todo.init)
+        filteredTodos()
+    }
+
+    func filteredTodos() {
+        let (startOfDay, endOfDay) = calculateDateRange(for: date)
+        let filteredTodos = todos.filter { todo in
+            return (todo.startDate >= startOfDay && todo.startDate <= endOfDay) ||
+            (todo.endDate >= startOfDay && todo.endDate <= endOfDay)
+        }
+        self.displayTodos = filteredTodos
+    }
+
+    private func calculateDateRange(for date: Date) -> (startOfDay: Date, endOfDay: Date) {
+        let startOfDay = Calendar.current.startOfDay(for: date)
+        let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)?.addingTimeInterval(-1) ?? startOfDay
+        return (startOfDay, endOfDay)
+    }
+
+    func sheetTodoForm(_ todo: Todo?) {
+        if let todo = todo {
+            let todoEntity = TodoEntity(
+                id: UUID(uuidString: todo.id) ?? .init(),
+                title: todo.title,
+                detail: todo.detail,
+                createdDate: Date.now,
+                startDate: todo.startDate,
+                endDate: todo.endDate
+            )
+            self.sheet = .todoForm(date, todoEntity)
+        } else {
+            self.sheet = .todoForm(date, nil)
+        }
+        
+    }
+}
+
+extension CalendarViewModel {
+    struct Todo: Hashable {
+        var id: String
+        var title: String
+        var detail: String
+        var startDate: Date
+        var endDate: Date
+        var displayDate: String
+    }
+    
+    enum Sheet: Identifiable, Hashable {
+        var id: Int { hashValue }
+        case todoForm(Date, TodoEntity?)
+    }
+}
+
+extension CalendarViewModel.Todo {
+    init(todo: TodoEntity) {
+        self.init(
+            id: todo.id.uuidString,
+            title: todo.title,
+            detail: todo.detail,
+            startDate: todo.startDate,
+            endDate: todo.endDate,
+            displayDate: "開始: \(todo.startDate.formatted("MM/dd HH:mm"))\n終了: \(todo.endDate.formatted("MM/dd HH:mm"))"
+        )
+    }
+}
